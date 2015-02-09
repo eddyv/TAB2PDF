c610d0e9db87229d88314f02131f10d06165672b
jenniferto <jento.jt@gmail.com>
2015-02-09 02:32:38 -0500

    more changes

---
+++ DrawOutput/src/main/java/com/drawpdf/drawoutput/Main.java
@@ -0,0 +1,27 @@
+/*
+ * this project can't distinguish simple bar and repeat bar
+ * 
+ */
+package com.drawpdf.drawoutput;
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.PdfContentByte;
+import com.itextpdf.text.pdf.PdfTemplate;
+import com.itextpdf.text.pdf.PdfWriter;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, DocumentException{
+        new drawOutput("sample1.txt", "P1.pdf");
+    }
+}
